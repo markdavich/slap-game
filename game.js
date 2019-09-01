c14f9c82e5969501578f715b5e40ac26f4530b93
Mark Davich <markdavich@gmail.com>
2019-09-01 11:53:15 -0600

    Done with the requirements

--- game.js
+++ game.js
@@ -26,7 +26,7 @@ class Modifier {
    * @param {number} winsToUse The number of wins a player needs before they can use the modifier
    * @param {number} allowedUseCount The number of times the player can use the mod
    */
-  constructor(modifies, shortName, value, description, winsToUse, allowedUseCount) {
+  constructor(modifies, shortName, value, description, winsToUse, allowedUseCount, key) {
     this._modifies = modifies;
     this._shortName = shortName;
     this._value = value;
@@ -34,6 +34,7 @@ class Modifier {
     this._winsToUse = winsToUse;
     this._allowedUseCount = allowedUseCount;
     this._totalMods = allowedUseCount;
+    this._key = key
   }
   /**
    * @returns MODIFIER_TYPE.DEFENSE | MODIFIER_TYPE.OFFENSE
@@ -67,27 +68,93 @@ class Modifier {
   }
 
   get allowedUseCount() {
-    return this._allowedUseCount
+    return this._totalMods
   }
 
-  getModValues(winCount, modType) {
+  /**
+   * 
+   * @param {Player} player 
+   * @param {boolean} inUse
+   */
+  setStatus(player, inUse) {
+
+    // This needs to set the checked and enabled
+    if (this._totalMods <= 0) {
+      inUse = false
+    }
+
+    let modIndex = player._modifiers.findIndex(key => key === this._key)
+
+    if (inUse && modIndex === -1) {
+      player._modifiers.push(this._key)
+    } else if (!inUse && modIndex > -1) {
+      player._modifiers.splice(modIndex, 1)
+    }
+  }
+
+  /**
+   * @param {Player} player 
+   * @param {number} modType MOD_TYPE.DEFENSE | MOD_TYPE.OFFENSE
+   * @returns {object} {template: string, value: number}
+   * --NOTE This function decreases _totalMods
+   */
+  getModValues(player, modType) {
     let result = { template: '', value: 0 }
 
     if (this._modifies !== modType) {
       return result
     }
 
-    if (winCount >= this._winsToUse && this._totalMods) {
+    if (player.winCount >= this._winsToUse && this._totalMods > 0) {
       this._totalMods--
+      this.setStatus(player, player._modifiers.findIndex(key => key === this._key) > -1)
       return {
         template: `<p>${this.description} (<b>${this._value}pts</b>)</p>`,
         value: modType === MODIFIER_TYPE.OFFENSE ?
           this._value : -this._value
       }
     }
-
     return result
   }
+
+  /** @param {Player} player */
+  modUnlocked(player) {
+    return player.winCount >= this.winsToUse && this._totalMods > 0 ?
+      '' :
+      'disabled'
+  }
+
+  /** @param {Player} player*/
+  modChecked(player) {
+    return player._modifiers.findIndex(mod => mod === this._key) > -1 && this._totalMods > 0 ?
+      'checked' :
+      ''
+  }
+
+  /**
+   * 
+   * @param {Player} player 
+   */
+  getTemplate(player) {
+    let key = this._key
+    let template = `
+        <div class="form-check ml-4" style="display: block;">
+          <input
+            id="cb-${key}"
+            onchange="onModChange(event, '${player.name}', '${key}')"
+            class="form-check-input"
+            type="checkbox"
+            ${this.modUnlocked(player)}
+            ${this.modChecked(player)}
+          >
+          <label class="form-check-label" for="defaultCheck1">
+            ${this._shortName} (${this._totalMods})
+          </label>
+        </div>
+      `
+    return template
+  }
+
 
 }
 
@@ -147,17 +214,16 @@ class Player {
     return this.health
   }
 
-  getModValues(winCount, modType) {
+  getModValues(modType) {
     let template = ''
     let modDamage = 0
-    this._modifiers.forEach(key => {
+    let keys = [...this._modifiers]
+    keys.forEach(key => {
       /** @type {Modifier} */
       let mod = this.modifier[key]
-      if (mod.modifies === MODIFIER_TYPE.OFFENSE) {
-        let modValues = mod.getModValues(winCount, modType)
-        template += modValues.template
-        modDamage += modValues.value
-      }
+      let modValues = mod.getModValues(this, modType)
+      template += modValues.template
+      modDamage += modValues.value
     })
 
     return {
@@ -172,52 +238,76 @@ class Player {
    * @param {Player} opponent 
    */
   attack(type, opponent) {
-    $('#attack-sentance')
-      .animate({ opacity: '0' }, 250)
+    let opponentDefenseId = `#${opponent._name.toLowerCase()}-defense`
+    let myDefenseId = `#${this._name.toLowerCase()}-defense`
+
+
+    // Clear attack and mod defense sentances
+    $('#attack-sentance').animate({ opacity: '0' }, 1)
+    $(opponentDefenseId).animate({ opacity: '0' }, 250)
+    $(myDefenseId).animate({ opacity: '0' }, 250)
+
+    let mySide
+    let theirSide
+
+    if (this._name === 'Human') {
+      mySide = 'justify-content-start'
+      theirSide = 'justify-content-end'
+    } else {
+      mySide = 'justify-content-end'
+      theirSide = 'justify-content-start'
+    }
+
+    $('#attack-sentance').removeClass(theirSide)
+    $('#attack-sentance').addClass(mySide)
+    $('#attack-row').removeClass(theirSide)
+    $('#attack-row').addClass(mySide)
+
+    // Clear the last attack sentance in the arean
+    // $('#attack-sentance').animate({ opacity: '0' }, 250)
 
     let attackValues = this._attacks[type]
 
-    let modValues = this.getModValues(
-      this._winCount, MODIFIER_TYPE.OFFENSE
-    )
+    let modValues = this.getModValues(MODIFIER_TYPE.OFFENSE)
+    let theirModValues = opponent.getModValues(MODIFIER_TYPE.DEFENSE)
 
-    let theirModValues = opponent.getModValues(
-      opponent._winCount, MODIFIER_TYPE.DEFENSE
-    )
-
+    // Load the current attack sentance
     modValues.value += attackValues.damage + theirModValues.value
     modValues.template += `<p><b>${attackValues.description} Total Damage ${modValues.value}</b></p>`
 
-    let defenseId = `#${this._name.toLowerCase()}-defense`
-
-    // dispaly modifier sentance + attack + Calculate total damage
-
-    // display opponents defense sentance
-    $(defenseId)
-      .html(theirModValues.template)
-      .animate({ opacity: '1' }, 250)
-      .delay(4000)
-      .animate({ opacity: '0' }, 250)
-      .html('')
-
-    $('#attack-sentance')
-      .html(modValues.template)
-      .animate({ opacity: '1' }, 250)
-      .delay(3000)
-
+    // Attack the opponent. This decreases their _health
+    // TODO This needs to decreas their defensive mod value if checked
     opponent.underGoAttack(modValues.value)
 
+    // Check if this player is a winner
+    // This resets everybodys _health and adds "Winner" attack sentance
     if (opponent.health <= 0) {
       this._winCount++
       this._health = 100
       opponent.health = 100
+      modValues.template += `<h2>${this._name} Wins!</h2>`
     }
 
+    // display opponents defense sentance
+    $(opponentDefenseId)
+      .html(theirModValues.template)
+      .animate({ opacity: '1' }, 250)
+
+    // Dispaly the attack sentance
+    $('#attack-sentance')
+      .html(modValues.template)
+      .delay(750)
+      .animate({ opacity: '1' }, 250)
+
+    // Set the title for each player.
+    // This shows their health
     this.setTitle()
     opponent.setTitle()
 
+    // Set attack object url by changing the class: SEE style.css
     $('#attack-object').attr('class', type)
 
+    // Animate the attack object
     $('#attack-object')
       .animate({ top: '88px' }, 700).delay(250)
       .animate({ top: '-88px' }, 700)
@@ -225,43 +315,46 @@ class Player {
     $('#battle-arena').delay(1500).animate({ opacity: '0' }, 250)
   }
 
-  modUnlocked(key) {
-    return this._winCount >= this._modifier[key].winsToUse ?
-      '' :
-      'disabled'
-  }
+  // modUnlocked(key) {
+  //   return this._winCount >= this._modifier[key].winsToUse ?
+  //     '' :
+  //     'disabled'
+  // }
 
-  modChecked(key) {
-    return this._modifiers.findIndex(mod => mod === key) > -1 ?
-      'checked' :
-      ''
-  }
+  // modChecked(key) {
+  //   return this._modifiers.findIndex(mod => mod === key) > -1 ?
+  //     'checked' :
+  //     ''
+  // }
 
   get ModifierTemplate() {
     let template = ''
 
     Object.keys(this._modifier).forEach(key => {
+      /** @type {Modifier} */
       let mod = this._modifier[key]
-      template += `
-        <div class="form-check ml-4" style="display: block;">
-          <input
-            id="cb-${key}"
-            onchange="onModChange(event, '${this._name}', '${key}')"
-            class="form-check-input"
-            type="checkbox"
-            ${this.modUnlocked(key)}
-            ${this.modChecked(key)}
-          >
-          <label class="form-check-label" for="defaultCheck1">
-            ${mod.shortName} (${mod.allowedUseCount})
-          </label>
-        </div>
-      `
+      template += mod.getTemplate(this)
+      // template += `
+      //   <div class="form-check ml-4" style="display: block;">
+      //     <input
+      //       id="cb-${key}"
+      //       onchange="onModChange(event, '${this._name}', '${key}')"
+      //       class="form-check-input"
+      //       type="checkbox"
+      //       ${this.modUnlocked(key)}
+      //       ${this.modChecked(key)}
+      //     >
+      //     <label class="form-check-label" for="defaultCheck1">
+      //       ${mod.shortName} (${mod.allowedUseCount})
+      //     </label>
+      //   </div>
+      // `
     })
     return template
   }
 
-  useMod(inUse, modKey) {
+  /** This adds or removes a mod key form _modifiers */
+  setMod(inUse, modKey) {
     let modIndex = this._modifiers.findIndex(key => key === modKey)
 
     if (inUse && modIndex === -1) {
@@ -287,19 +380,19 @@ class Cat extends Player {
     this._modifier = {
       takeSweetTime: new Modifier(
         MODIFIER_TYPE.OFFENSE, 'Take Sweet Time', 5,
-        'Irritate the human by testing their patience then...', 1, 2
+        'Irritate the human by testing their patience then...', 1, 2, 'takeSweetTime'
       ),
       ignoreHuman: new Modifier(
         MODIFIER_TYPE.OFFENSE, 'Ignore Human', 10,
-        'Lower the human\'s confidence by making them feel unimportant, then...', 2, 1
+        'Lower the human\'s confidence by making them feel unimportant, then...', 2, 1, 'ignoreHuman'
       ),
       purr: new Modifier(
         MODIFIER_TYPE.OFFENSE, 'Purrrrrr...', 15,
-        'Make the human totally let their guard down, then...', 3, 1
+        'Make the human totally let their guard down, then...', 3, 1, 'purr'
       ),
       bolt: new Modifier(
         MODIFIER_TYPE.DEFENSE, 'Try to escape', 4,
-        'With no indication... Twist and bolt!', 1, 2
+        'With no indication... Twist and bolt!', 1, 500, 'bolt'
       )
     }
   }
@@ -328,19 +421,19 @@ class Human extends Player {
     this._modifier = {
       giveCatNip: new Modifier(
         MODIFIER_TYPE.OFFENSE, 'Give the kitty some dope', 5,
-        'Subdue the cat with an inhalent, then...', 1, 2
+        'Subdue the cat with an inhalent, then...', 1, 2, 'giveCatNip'
       ),
       useLaser: new Modifier(
         MODIFIER_TYPE.OFFENSE, 'Use laser pointer', 10,
-        'Using a laser pointer, immeadiatly divert the cats attention, then...', 2, 1
+        'Using a laser pointer, immeadiatly divert the cats attention, then...', 2, 1, 'useLaser'
       ),
       openBox: new Modifier(
         MODIFIER_TYPE.OFFENSE, 'Catdoras Box', 15,
-        'Spark the cat\'s curiosity by oening a box.\nWhile the cat is peering into the abyss...', 3, 1
+        'Spark the cat\'s curiosity by oening a box.\nWhile the cat is peering into the abyss...', 3, 1, 'openBox'
       ),
       sleveDown: new Modifier(
         MODIFIER_TYPE.DEFENSE, 'Armor', 4,
-        'Roll your sleve down to protect from scratches', 1, 2
+        'Roll your sleve down to protect from scratches', 1, 2, 'sleveDown'
       )
     }
   }
@@ -374,7 +467,6 @@ function attack(event, attacker, type) {
       human.attack(type, cat)
       break
     case 'cat':
-      debugger
       cat.attack(type, human)
       break
   }
@@ -392,9 +484,11 @@ function onModChange(event, playerName, modKey) {
   let inUse = event.target.checked
   switch (playerName) {
     case 'Cat':
-      cat.useMod(inUse, modKey)
+      cat.setMod(inUse, modKey)
+      break
     case 'Human':
-      human.useMod(inUse, modKey)
+      human.setMod(inUse, modKey)
+      break
   }
 }
 
